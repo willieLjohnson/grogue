6cbf1300dc08b19ff541b52c6ac75811651791dd
Willie Johnson <liwa.johnson@gmail.com>
2022-12-16 14:47:08 -0500

    add collision detection for playermovesystem

--- playermovesystem.go
+++ playermovesystem.go
@@ -3,27 +3,34 @@ package main
 import "github.com/hajimehoshi/ebiten/v2"
 
 func TryMovePlayer(g *Game) {
-  players := g.WorldTags[PlayerTag]
-  x := 0
-  y := 0
-  if ebiten.IsKeyPressed((ebiten.KeyUp)) {
-    y = -1
-  }
+	players := g.WorldTags[PlayerTag]
+	x := 0
+	y := 0
+	if ebiten.IsKeyPressed((ebiten.KeyUp)) {
+		y = -1
+	}
 
-  if ebiten.IsKeyPressed((ebiten.KeyDown)) {
-    y = 1
-  }
+	if ebiten.IsKeyPressed((ebiten.KeyDown)) {
+		y = 1
+	}
 
-  if ebiten.IsKeyPressed((ebiten.KeyLeft)) {
-    x = -1 
-  }
-  if ebiten.IsKeyPressed((ebiten.KeyRight)) {
-    x = 1 
-  }
+	if ebiten.IsKeyPressed((ebiten.KeyLeft)) {
+		x = -1
+	}
+	if ebiten.IsKeyPressed((ebiten.KeyRight)) {
+		x = 1
+	}
 
-  for _, result := range g.World.Query(players) {
-    pos := result.Components[position].(*Position)
-    pos.X += x
-    pos.Y += y
-  }
+	level := g.Map.CurrentLevel
+
+	for _, result := range g.World.Query(players) {
+		pos := result.Components[position].(*Position)
+		index := level.GetIndexFromXY(pos.X+x, pos.Y+y)
+
+		tile := level.Tiles[index]
+		if tile.Blocked != true {
+			pos.X += x
+			pos.Y += y
+		}
+	}
 }
